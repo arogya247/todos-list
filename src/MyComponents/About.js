b439224d0d79a8814d8694d1e15fc9456bf69ffe
arogya <arogya247@gmail.com>
2021-09-27 17:33:49 +0530

    completed

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component. 
+            I dont even kknow why i am doing this. 
+            Lets see how it goes. 
+        </div>
+    )
+}
